9dee926358c2f112d1becf35d9afb80a52e9e047
Fernando Riveros <fer96nando@gmail.com>
2022-11-16 00:43:52 -0600

    [Commit 2] Fixed assertion for alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -4,11 +4,15 @@
 int alertFailureCount = 0;
 
 int networkAlertStub(float celcius) {
-    std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
     // Return 200 for ok
     // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
+    std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
+    if(celcius > 200){
+        return 500;
+    }
+    else{
+        return 200;
+    }
 }
 
 void alertInCelcius(float farenheit) {
@@ -24,8 +28,10 @@ void alertInCelcius(float farenheit) {
 }
 
 int main() {
-    alertInCelcius(400.5);
-    alertInCelcius(303.6);
+    alertInCelcius(303.6); // OK
+    assert(alertFailureCount == 0);
+    alertInCelcius(400.5); // NOK
+    assert(alertFailureCount == 1);
     std::cout << alertFailureCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
     return 0;
